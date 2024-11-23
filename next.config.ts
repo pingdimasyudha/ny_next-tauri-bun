05ac74b8e80c71edde4ebb2f559fd727d6ed6ff3
Dimas Yudha Pratama <ping@dimasyudha.com>
2024-11-24 04:24:16 +0700

    feat: initialize Rust-based native backend with `Tauri` for cross-platform support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "build",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "http://localhost:3000",
 };
 
 export default nextConfig;
