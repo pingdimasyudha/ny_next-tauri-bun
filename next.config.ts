149a4b6aaf5e79b909a6aae54ad635d8e7e640a1
Dimas Yudha Pratama <ping@dimasyudha.com>
2024-11-24 10:40:42 +0700

    fix: properly resolve asset paths on the server

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const internalHost = process.env.TAURI_DEV_HOST || "localhost";
+
 const nextConfig: NextConfig = {
   distDir: "build",
   output: "export",
   images: {
     unoptimized: true,
   },
-  assetPrefix: "http://localhost:3000",
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
